fixed bug in rotation

src/physics/geometry.py
@@ -16,7 +16,7 @@ def rotate_coordinates(subhalo, rot_vector):
         theta = np.nan_to_num(np.arccos(np.dot(v/lg.norm(v), old_basis[:, 2]))) #get angle between new z axis and old z axis
         phi = np.nan_to_num(np.arccos(np.dot(v_xy/lg.norm(v_xy), old_basis[:, 0]))) #get angle between projected new z axis and old x axis
         #Rotate phi radians about z and theta radians about y
-        r = R.from_euler('zyx', [phi, theta, 0])
+        r = R.from_euler('zyx', [phi, -theta, 0])
         rot_matrix = np.array(r.as_matrix()) #convert to numpy array
         return rot_matrix
 
@@ -43,9 +43,13 @@ def rotate_pos_vel(subhalo, rot_vector):
         """
         old_basis = np.identity(3)
         v = new_z_axis
-        v_xy = np.array([v[0], v[1], 0])
-        theta = np.nan_to_num(np.arccos(np.dot(v/lg.norm(v), old_basis[:, 2]))) #get angle between new z axis and old z axis
-        phi = np.nan_to_num(np.arccos(np.dot(v_xy/lg.norm(v_xy), old_basis[:, 0]))) #get angle between projected new z axis and old x axis
+        if lg.norm(v) > 0: #Check that the new z-axis is non-zero
+            v_xy = np.array([v[0], v[1], 0])
+            theta = np.arccos(np.dot(v/lg.norm(v), old_basis[:, 2])) #get angle between new z axis and old z axis
+            phi = np.arccos(np.dot(v_xy/lg.norm(v_xy), old_basis[:, 0])) #get angle between projected new z axis and old x axis
+        else:
+            theta = 0
+            phi = 0
         #Rotate phi radians about z and theta radians about y
         r = R.from_euler('zyx', [phi, theta, 0])
         rot_matrix = np.array(r.as_matrix()) #convert to numpy array

src/physics/properties.py
@@ -120,7 +120,7 @@ def half_mass_radius(subhalo, catalogue):
     catalogue[rad_key] = halfmass_rad #save to catalogue
     return catalogue
 
-def max_ang_momentum(subhalo, catalogue):
+def ang_momentum(subhalo, catalogue):
     r_max = 2*catalogue["SubhaloHalfmassRadStellar"][0]
     temp1 = subhalo[subhalo["r"] > 0] #remove central particle
     temp = temp1[temp1["r"] < r_max].copy(deep=True)
@@ -128,11 +128,15 @@ def max_ang_momentum(subhalo, catalogue):
     p = np.array([np.array(temp["Vx"]*m), np.array(temp["Vy"]*m), np.array(temp["Vz"]*m)])
     r = np.array([np.array(temp["x"]), np.array(temp["y"]), np.array(temp["z"])])
     l = np.cross(np.transpose(r), np.transpose(p))
+    L = np.sum(l, axis=0)
     J = np.sum(l, axis=0)/np.sum(m)
-    j_dir = J/lg.norm(J)
-    catalogue["RotationAxisX"] = j_dir[0]
-    catalogue["RotationAxisY"] = j_dir[1]
-    catalogue["RotationAxisZ"] = j_dir[2]
+    catalogue["AngularMomentumX"] = L[0]
+    catalogue["AngularMomentumY"] = L[1]
+    catalogue["AngularMomentumZ"] = L[2]
+
+    catalogue["SpecificAngularMomentumX"] = J[0]
+    catalogue["SpecificAngularMomentumY"] = J[1]
+    catalogue["SpecificAngularMomentumZ"] = J[2]
     return catalogue
 
 def rot_energy(subhalo, catalogue):